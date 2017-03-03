c9d9634aafabccea074fea3782889b4a5a6a232e
rishaav <rsharma@knox.edu>
2017-03-03 14:47:20 -0600

    Add files via upload

--- parse.rb
+++ parse.rb
@@ -5,11 +5,19 @@ require 'nokogiri'
 def parseXML(filename)
   page = Nokogiri::XML(open(filename))
   puts page.class
+  page.css('//ssn//p').each do |a|
+   start =  a['strt'].to_i
+    stop = a['stp'].to_i
+    puts a['qn'] + " " + "Start Time: " + a['strt'] + " " + "Stop Time: " + a['stp']
+
+
   page.css('//ssn//p//v').each do |e|
-    puts e['id']
+
+    puts e['id'] + " " + "Given answer: " + e['ans']
+    end
   end
 end
 
 if __FILE__ == $0
-  parseXML('CS141Sec1/SessionData/L1609161028.xml')
+  parseXML('CS 141 FALL 2016/SessionData/L1609211319.xml')
 end
